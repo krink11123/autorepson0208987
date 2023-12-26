2e13b6d9c0c6fc76e3ad7ce06e8cf96aea389380
krink11123 <154920904+krink11123@users.noreply.github.com>
2023-12-26 19:41:28 +0100

    Add files via upload

---
+++ chatgpt.py
@@ -0,0 +1,131 @@
+import openai
+import json
+import time
+from telethon import TelegramClient, events
+
+global gpt_key
+global prompt
+global cashapp
+global bitcoin
+global litecoin
+global ethereum
+global api_id
+global api_hash
+
+
+def load_config():
+    global gpt_key
+    global prompt
+    global cashapp
+    global bitcoin
+    global litecoin
+    global ethereum
+    global api_id
+    global api_hash
+    with open("responderconfig.json", 'r') as f:
+        data = json.load(f)
+        gpt_key = data['gpt_key']
+        prompt = data['prompt']
+        cashapp = data['cashapp']
+        bitcoin = data['bitcoin']
+        litecoin = data['litecoin']
+        ethereum = data['ethereum']
+        api_id = data['api_id']
+        api_hash = data['api_hash']
+
+    prompt = prompt.replace("[chashapp]", cashapp).replace("[bitcoin]", bitcoin).replace("[litecoin]", litecoin).replace("[ethereum]", ethereum)
+
+
+
+def chat_with_gpt3(session, prompt, model="gpt-3.5-turbo-16k-0613", max_tokens=150):
+    try:
+        # Append the new user message to the existing session
+        session.append({"role": "user", "content": prompt})
+        
+        # Generate a response based on the updated session
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=session,
+            max_tokens=max_tokens
+        )
+        
+        # Return the response and updated session
+        return response['choices'][0]['message']['content'].strip(), session
+    except Exception as e:
+        return str(e), session
+
+def get_session(user_id):
+    with open("sessions.json", 'r') as f:
+        data = json.load(f)
+    user_found = False
+    for session in data:
+        if user_id == session['user_id']:
+            return session['session_content'], True
+
+    new_session = {
+        "user_id" : user_id,
+        "session_content" : []
+    }
+
+    data.append(new_session)
+
+    with open("sessions.json", 'w') as f:
+        json.dump(data, f, indent=4) 
+
+
+    return [{"role": "system", "content": "You are a helpful assistant."}], False
+
+def save_session(user_id, sessiondata):
+    with open("sessions.json", 'r') as f:
+        data = json.load(f)
+    for session in data:
+        if user_id == session['user_id']:
+            session['session_content'] = sessiondata
+
+    with open("sessions.json", 'w') as f:
+        json.dump(data, f, indent=4) 
+
+
+
+
+load_config()
+
+openai.api_key = gpt_key
+
+client = TelegramClient('auto_respoder', api_id, api_hash)
+
+
+@client.on(events.NewMessage(incoming=True))
+async def handle_new_message(event):
+    if event.is_private:
+        #print(event.message.message)
+        while True:
+            load_config()
+            session, already_chatted = get_session(event.sender_id)
+            print(already_chatted)
+            if already_chatted == False:
+                automated_response, session = chat_with_gpt3(session, f"{prompt} {event.message.message} .How much is vip whats in vip and how can i pay?")
+            else:
+                automated_response, session = chat_with_gpt3(session, f"Remember to not begin with hey, hi, hello or anything like that be straight forward and short. Also if someone says something like (cashapp, bitcoin, ethereum) give them the details to that payment methode. With all those instructions in min awnser the following question: {event.message.message}")
+            if "Rate limit reached" not in automated_response:
+                break
+            else:
+                print("TIMEOUT!!!")
+                time.sleep(25)
+        save_session(event.sender_id, session)
+        await event.respond(automated_response)
+
+with client:
+    client.run_until_disconnected()
+
+
+
+
+
+
+#openai.api_key = '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'
+#
+## Example of chatting with GPT-3
+#user_input = "Who was adolg hitler"
+#response = chat_with_gpt3(user_input)
+#print("GPT-3:", response)
